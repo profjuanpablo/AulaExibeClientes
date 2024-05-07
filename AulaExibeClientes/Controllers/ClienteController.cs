2395e21622bd8a5ed1fd63e4403ae612eaf18548
profjuanpablo <prof.juanpablo@gmail.com>
2024-05-07 15:42:45 -0300

    Listar exibindo cada elemento pelo id

---
+++ AulaExibeClientes/Controllers/ClienteController.cs
@@ -0,0 +1,20 @@
+﻿using AulaExibeClientes.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AulaExibeClientes.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public IActionResult Index()
+        {
+            //Simular lista de clientes
+            var clientes = new List<Cliente> {
+                new Cliente { Id = 1, Nome="Mangabeira", Email="mangabeira@gmail.com"},
+                new Cliente { Id = 2, Nome="zandra", Email="zandra@gmail.com"},
+                new Cliente { Id = 3, Nome="Yasmin", Email="yasmin@gmail.com"},
+            };
+            ViewBag.Clientes = clientes;
+            return View();
+        }
+    }
+}
